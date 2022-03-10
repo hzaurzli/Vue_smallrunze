3c246f53d24f69310ab88494227ca91d62756ec6
Small runze <yun_act@163.com>
2022-03-11 00:35:25 +0800

    Add files via upload

--- User/rzli/src/components/on.vue
+++ User/rzli/src/components/on.vue
@@ -1,7 +1,11 @@
 <template>
-	<div>
+	<div id="app">
 		组件A
 		<h2>{{ data }}</h2>
+    <div v-html="word"></div>
+    <div>
+      <img :src= "img">
+    </div>
 	</div>
 </template>
 
@@ -10,11 +14,23 @@ export default {
 	name: 'comA',
 	data() {
 		return {
-			data: ''
+			data: '',
+      word:'',
+      img: ''
+
 		}
 	},
 	methods: {},
+  // 设置this.word动态loading效果
+  created(){
+	  var word = '<p>aaaaa</p>';
+	  this.word = word;
+	  this.img = "http://localhost:8080/src/assets/logo.png";
+	  console.log(word);
+  },
 	mounted() {
+	  this.word = '';
+	  this.img = '';
 		this.$bus.$on('sendToA', data => {
 			this.data = data;
       console.log(this.data)
